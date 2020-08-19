66152bc7d5d6ac010bf6a15183256efc9f5d581f
Mohit Rane <mrrane99@gmail.com>
2020-08-19 21:46:33 +0530

    Node MongoDB Example 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -10,6 +11,30 @@ MongoClient.connect(url, (err, client)=>{
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
+
+    dboper.insertDocument(db, {name: "Vadonut", description: "Test"}, 'dishes', (result)=>{
+        console.log('Insert Document: \n', result.ops);
+
+        dboper.findDocument(db, 'dishes', (docs)=>{
+            console.log('Found Documents:\n', docs);
+
+            dboper.updateDocument(db, {name: 'Vadonut'}, {description: "Updated Test"}, 'dishes', (result)=>{
+                console.log('Updated Document:\n', result.result);
+
+                dboper.findDocument(db, 'dishes', (docs)=>{
+                    console.log('Found Documents:\n', docs);
+
+                    db.dropCollection('dishes', (result)=>{
+                        console.log('Dropped Collection: ', result)
+
+                        client.close()
+                    })
+                });
+            });
+        });
+    });
+
+    /*
     const collection = db.collection('dishes');
 
     collection.insertOne({"name":"Uthappizza", "description":"test"}, (err, result)=>{
@@ -31,4 +56,5 @@ MongoClient.connect(url, (err, client)=>{
             })
         })
     })
+    */
 })
