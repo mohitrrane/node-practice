9fe4c3cbba01333a966fe7f3392aa26c89a1bc3f
Mohit Rane <mrrane99@gmail.com>
2020-08-22 17:03:34 +0530

    mongoose part 3 (adding subdocument to array in document)

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,16 +8,32 @@ connect.then((db)=>{
     console.log('Connected correctly to server');
 
     Dishes.create({
-        name: 'Uthappizza',
-        description: 'test'
+        name: 'Uthappiz',
+        description: 'test',
+        comments: []
     })
     .then((dish)=>{
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        }, {
+            new: true
+        }).exec();
     })
-    .then((dishes)=>{
-        console.log(dishes);
+    .then((dish)=>{
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish)=>{
+        console.log(dish);
 
         return Dishes.remove({});
     })
